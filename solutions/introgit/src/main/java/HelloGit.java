b5a6241ebbcb0a820de52453123da14c81f3fe63
torokand <torok.andras@hotmail.com>
2021-04-12 23:23:56 +0200

    Hello GitHub!

--- solutions/introgit/src/main/java/HelloGit.java
+++ solutions/introgit/src/main/java/HelloGit.java
@@ -2,6 +2,6 @@ public class HelloGit {
 
     public static void main(String[] args) {
         // System.out.println("Hello Git 2!");
-        System.out.println("Hello IDEA + Git!");
+        System.out.println("Hello GitHub!");
     }
 }
