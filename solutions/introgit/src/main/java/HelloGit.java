a32c81ce39b573f6849af2f7e5cd571b327b39ba
torokand <torok.andras@hotmail.com>
2021-04-12 23:13:22 +0200

    T360 gyakorlas Hello IDEA + Git.

--- solutions/introgit/src/main/java/HelloGit.java
+++ solutions/introgit/src/main/java/HelloGit.java
@@ -1,7 +1,7 @@
 public class HelloGit {
 
-  public static void main(String[] args) {
-    // System.out.println("Hello Git 2!");
-    System.out.println("Hello Git!");
-  }
+    public static void main(String[] args) {
+        // System.out.println("Hello Git 2!");
+        System.out.println("Hello IDEA + Git!");
+    }
 }
